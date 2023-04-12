319ef30c54c1c687dcf023624e4fab26b19314e7
Wes Hunt <wesleyahunt@gmail.com>
2023-04-12 13:13:18 -0700

    Update indentation

--- pages/index.js
+++ pages/index.js
@@ -3,56 +3,56 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+    const [animalInput, setAnimalInput] = useState("");
+    const [result, setResult] = useState();
 
-  async function onSubmit(event) {
-    event.preventDefault();
-    try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ animal: animalInput }),
-      });
+    async function onSubmit(event) {
+        event.preventDefault();
+        try {
+            const response = await fetch("/api/generate", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ animal: animalInput }),
+            });
 
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
-      }
+            const data = await response.json();
+            if (response.status !== 200) {
+                throw data.error || new Error(`Request failed with status ${response.status}`);
+            }
 
-      setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
-      // Consider implementing your own error handling logic here
-      console.error(error);
-      alert(error.message);
+            setResult(data.result);
+            setAnimalInput("");
+        } catch(error) {
+            // Consider implementing your own error handling logic here
+            console.error(error);
+            alert(error.message);
+        }
     }
-  }
 
-  return (
-    <div>
-      <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
-      </Head>
+    return (
+        <div>
+          <Head>
+            <title>OpenAI Quickstart</title>
+            <link rel="icon" href="/dog.png" />
+          </Head>
 
-      <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
-        </form>
-        <div className={styles.result}>{result}</div>
-      </main>
-    </div>
-  );
+          <main className={styles.main}>
+            <img src="/dog.png" className={styles.icon} />
+            <h3>Name my pet</h3>
+            <form onSubmit={onSubmit}>
+              <input
+                type="text"
+                name="animal"
+                placeholder="Enter an animal"
+                value={animalInput}
+                onChange={(e) => setAnimalInput(e.target.value)}
+              />
+              <input type="submit" value="Generate names" />
+            </form>
+            <div className={styles.result}>{result}</div>
+          </main>
+        </div>
+    );
 }
